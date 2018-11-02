42c41fd616784b398405d3f536818d19c5bd00e5
Astrian <mingriqd@gmail.com>
2018-11-02 21:59:44 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+let express = require('express')
+let app = express()
+let fs = require('fs')
+let https = require('https')
+let privateKey  = fs.readFileSync('private.pem', 'utf8')
+let certificate = fs.readFileSync('file.crt', 'utf8')
+let credentials = {key: privateKey, cert: certificate}
+
+let httpsServer = https.createServer(credentials, app)
+
+httpsServer.listen(443, () => {
+    console.log('HTTPS Server is running on: https://localhost:%s', 443)
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
